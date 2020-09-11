0ab52407145cb61d916a049a6b79eef4b8c94d25
mitjapotocin <mitja.potocin@gmail.com>
2020-09-12 01:18:58 +0200

    Created basic svg circle

--- src/js/CircularSlider.js
+++ src/js/CircularSlider.js
@@ -1,5 +1,37 @@
 export default class CircularSlider {
   constructor(options) {
-    console.log('Slider')
+    this.container = options.container
+    this.radius = options.radius
+    this.strokeWidth = 30
+    this.createCircle()
+  }
+
+  createCircle() {
+    const sliderSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    this.setAttributes(sliderSvg, {
+      width: this.radius * 2 + this.strokeWidth,
+      height: this.radius * 2 + this.strokeWidth,
+    })
+
+    const baseCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+    this.setAttributes(baseCircle, {
+      cx: this.radius + this.strokeWidth / 2,
+      cy: this.radius + this.strokeWidth / 2,
+      r: this.radius,
+      stroke: 'gray',
+      fill: 'none',
+      'stroke-width': this.strokeWidth,
+      'stroke-dasharray': '50 2',
+    })
+
+    this.container.classList.add('circular-slider-container')
+    sliderSvg.append(baseCircle)
+    this.container.append(sliderSvg)
+  }
+
+  setAttributes(el, attributeAndValue) {
+    Object.entries(attributeAndValue).forEach(([attribute, value]) => {
+      el.setAttribute(attribute, value)
+    })
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 import CircularSlider from './CircularSlider'
 
-const slider = new CircularSlider('')
+const sliderContainer = document.querySelector('#slider-container')
+const slider = new CircularSlider({
+  container: sliderContainer,
+  color: '#bada55',
+  minValue: 10,
+  maxValue: 1000,
+  step: 10,
+  radius: 200,
+})
+const slider2 = new CircularSlider({
+  container: sliderContainer,
+  color: '#bada55',
+  minValue: 10,
+  maxValue: 1000,
+  step: 10,
+  radius: 100,
+})
